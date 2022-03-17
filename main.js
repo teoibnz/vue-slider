e165c1490969f01400a47e7ea25e21c81f9a18c7
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-17 19:01:28 +0100

    inizializzazione esercizio

---
+++ main.js
@@ -0,0 +1,29 @@
+const app = new Vue (
+    {
+        el : '#app',
+
+        activeElement : 0,
+        data : {
+            images : [
+                {
+                    nome : 'First Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-1.jpeg', 
+                },
+                {
+                    nome : 'Second Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-2.jpeg',
+                },
+                {
+                    nome : 'Third Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-3.jpeg',
+                },
+                {
+                    nome : 'Fourth Guitar',
+                    url : '/Users/Matteo/Desktop/BOOLEAN/CLASSE 55/vue-slider/img/fender-img-strato.jpeg',
+                },
+            ]            
+        }
+        
+    } 
+);
+
